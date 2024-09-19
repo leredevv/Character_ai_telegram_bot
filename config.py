ea5f64d0277c62471574359afbccb4a8cd6ca369
leredev <135429895+leredevv@users.noreply.github.com>
2024-09-19 22:47:52 +1000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Main configuration file
+
+CAI_TOKEN="a2e964850d61e4f78b2877c153ff210eaca79a5e" # Character.ai token
+CAI_CHAR="1SFPNC5xmBfz7GUJYzNZnQlvKLmdOSbeJbyybgeJdQY" # Character.ai character ID 
+BOT_TOKEN="6540116725:AAGmWPNpd7ljJXRVB_Iv0CLBsj81dzIgb5I" # Telegram bot token
+TG_ID=6714089456 # Your Telegram account ID
